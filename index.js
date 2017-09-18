01aca6334eecfd2bafdf65b167217dadaec11669
Micael Levi <mllc@icomp.ufam.edu.br>
2017-09-18 18:20:12 -0400

    dotenv path

--- index.js
+++ index.js
@@ -22,7 +22,10 @@
 
 
 /********************************************************/
-require('dotenv').config()
+const path = require('path')
+require('dotenv').load({
+  path: path.join(__dirname, '.env')
+});
 const Telegraf = require('telegraf')
 const app = new Telegraf(process.env.BOT_TOKEN)
 /********************************************************/
