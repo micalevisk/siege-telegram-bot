be9a76223b7af470a601f2e442b9c5db253b5829
Micael Levi <mllc@icomp.ufam.edu.br>
2017-12-11 03:32:30 -0400

    add uso de questao/6

--- src/brain/aprendizado-utils.js
+++ src/brain/aprendizado-utils.js
@@ -1,8 +1,11 @@
 const { PATH_CONHECIMENTOS_EXTERNOS } = require('./prolog-controller/config')
 
 const strQueriesAprendizado = {
-  salvarQuestao: (pergunta, resposta, usernameAutor, idAutor) => `§salvar_questao("${pergunta}", "${resposta}", "${usernameAutor}", ${idAutor}, "${PATH_CONHECIMENTOS_EXTERNOS}")`,
+
+  salvarQuestao: (pergunta, resposta, usernameAutor, idAutor, ano) => `§salvar_questao("${pergunta}", "${resposta}", "${usernameAutor}", ${idAutor}, ${ano}, "${PATH_CONHECIMENTOS_EXTERNOS}")`,
+
   incrementarVoto: pergunta => `§incrementar_voto("${pergunta}", Votos, "${PATH_CONHECIMENTOS_EXTERNOS}")`,
+
 }
 
 
